694a3da8ddfb129644ae9d420d693fa4c3785aac
cristirotaru1 <cri.rotaru2001@gmail.com>
2025-01-05 23:23:41 +0200

    Added project

---
+++ TravelPlanner/TravelPlanner/Models/AccommodationReviewViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelPlanner.Models
+{
+    public class AccommodationReviewViewModel
+    {
+        public int ReviewId { get; set; }
+        public int AccommodationId { get; set; }
+        public string AccommodationName { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public string FullName { get; set; }
+    }
+}
---
+++ TravelPlanner/TravelPlanner/Models/ActivityReviewViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelPlanner.Models
+{
+    public class ActivityReviewViewModel
+    {
+        public int ReviewId { get; set; }
+        public int ActivityId { get; set; }
+        public string ActivityName { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public string FullName { get; set; }
+    }
+}
